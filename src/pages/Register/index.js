362ff9376e67dbcd12524c8c39c09c966270ef3f
Guilherme-07062002 <guilherme762002@gmail.com>
2023-06-07 13:27:05 -0300

    implementando arquitetura de diretórios

---
+++ src/pages/Register/index.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
+import styles from "./style";
+
+export default function Register() {
+  return (
+    <View style={styles.container}>
+      <Text>Texto</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
