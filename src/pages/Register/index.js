9c805edbbaab74d99e0566212685c3be9fffe300
Guilherme-07062002 <guilherme762002@gmail.com>
2023-06-07 13:44:02 -0300

    posicionamento dos componentes

--- src/pages/Register/index.js
+++ src/pages/Register/index.js
@@ -1,11 +1,42 @@
 import { StatusBar } from "expo-status-bar";
-import { Text, View } from "react-native";
+import { Text, View, TextInput, TouchableOpacity } from "react-native";
 import styles from "./style";
 
 export default function Register() {
   return (
     <View style={styles.container}>
-      <Text>Texto</Text>
+      <Text>Tela de Cadastro</Text>
+
+      <View style={styles.form}>
+        <View style={styles.inputField}>
+          <Text>Nome:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu nome"
+          ></TextInput>
+        </View>
+
+        <View style={styles.inputField}>
+          <Text>Email: </Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite seu email"
+          ></TextInput>
+        </View>
+
+        <View style={styles.inputField}>
+          <Text>Senha:</Text>
+          <TextInput
+            style={styles.input}
+            placeholder="Digite sua senha"
+            secureTextEntry
+          ></TextInput>
+        </View>
+
+        <TouchableOpacity style={styles.button}>
+          <Text>Cadastrar</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
--- src/pages/Register/style.js
+++ src/pages/Register/style.js
@@ -6,7 +6,27 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    gap: 60,
   },
+  form: {
+    gap: 30,
+    width: '60%'
+  },
+  inputField:{
+    display: 'flex',
+    flexDirection: 'row',
+    gap: 20
+  },
+  input: {
+    borderColor: 'black',
+    height: 30,
+    width: '100%',
+  },
+  button: {
+    justifyContent: 'center',
+    textAlign: 'center',
+    alignItems: 'center'
+  }
 });
 
 export default styles;
